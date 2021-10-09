7905edbe5d1a83c9b80c2d011e622136d1585f83
gilles-danneels <71772754+gilles-danneels@users.noreply.github.com>
2021-10-09 14:29:22 +0200

    fixed bg

--- pages/[category].js
+++ pages/[category].js
@@ -1,14 +1,29 @@
 import { useRouter  } from "next/router";
 import styles from '../styles/Category.module.css'
 
-
-const CategoryDishes = () => {
+const CategoryDishes = ({data}) => {
     const router = useRouter()
     const { category } = router.query
 
+
     return(
         <>
-          <h1>Type: {category}</h1>
+          <div className={styles.wrapper_ingredients_page}>
+            <div className={styles.ontent_recipes}>
+             <h1 className={styles.title}>{category} recipes</h1>
+             <div className={styles.container}>
+              {/* load data from datocms */}
+              <div className={styles.recipe_card}>
+
+              </div>
+              <div className={styles.recipe_card}>
+
+              </div>
+
+              
+             </div>
+            </div>
+          </div>
         </>
     )
     
