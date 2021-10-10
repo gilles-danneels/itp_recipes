6453c35c7c40f5b54da78014ed6ef6568139336f
gilles-danneels <71772754+gilles-danneels@users.noreply.github.com>
2021-10-10 13:31:36 +0200

    full responsive site

--- pages/[category].js
+++ pages/[category].js
@@ -18,7 +18,7 @@ const CategoryDishes = ({ recipes }) => {
             {/* load data from datocms */}
             {recipes.map((recipe) => (
               <div className={styles.recipe_card}>
-                <h1>{recipe.title}</h1>
+                <h1 className={styles.recipe_title}>{recipe.title}</h1>
                 <div className={styles.info}>
                   <p><FontAwesomeIcon icon={faClock} /> {recipe.bereidingstijd} min</p>
                   <p><FontAwesomeIcon icon={faUser} /> {recipe.aantalPersonen} personen</p>
@@ -42,48 +42,6 @@ const CategoryDishes = ({ recipes }) => {
               </div>
             ))
             }
-            {/* <div className={styles.recipe_card}>
-                <h1>Steak tartaar met frietjes</h1>
-                <div className={styles.info}>
-                    <p><FontAwesomeIcon icon={faClock} /> 60 min</p>
-                    <p><FontAwesomeIcon icon={faUser} /> 4 personen</p>
-                </div>
-                <div className={styles.ingredients}>
-                  <div>
-                    <h1>Ingrediënten</h1>
-                    <p>
-                      frietaardappelen 1kg, rund (pelee royale of bavette) 600g, sjalot 1, 
-                      een flinke scheut dragonazijn, peterselie 1 bussel, kappertjes 2 koffielepels, 
-                      augurken (klein) enkele, Tabasco enkele druppels, worcestersaus enkele druppels, 
-                      mayonaise 1 eetlepel, ketchup 1 eetlepel, peper en zout, dragon enkele takjes, kropsla 
-                      enkele blaadjes
-                    </p>
-                  </div>
-                </div>
-                <img ></img>
-                <div className={styles.prep_desc}>
-                  <div>
-                    <h1>Bereidingswijze</h1>
-                    <p>
-                      1. Verwarm de friteuse voor op 145°C.
-                      2. Schil de aardappelen, snij ze tot frieten en spoel ze met water. Laat ze kort uitlekken op een handdoek.
-                      3. Pocheer de frietjes in de friteuse tot ze gaar zijn. Laat ze nadien even afkoelen in de diepvriezer. Verhoog de temperatuur van de friteuse naar 175°C.
-                      4. Snijd het vlees eerst in plakjes en daarna heel fijn met 2 messen.
-                      5. Pel de sjalot en snipper één helft fijn. Snijd de andere helft in ringetjes voor de sla. Leg de ringetjes in een kom met dragonazijn en laat even trekken. Voeg de versnipperde sjalot toe aan het fijngemalen vlees.
-                      6. Hak ¼ van de bussel peterselie fijn. Snijd de kappertjes en de augurkjes fijn. Voeg toe aan het vlees.
-                      7. Breng het vlees op smaak met tabasco, worcestersaus, de mayonaise en de ketchup. Begin steeds met een eetlepel en bouw zo verder op. Kruid met peper en zout en meng de tartaar. Laat de tartaar even rusten in de koelkast.
-                      8. Snijd  ¼ van de bussel peterselie en de dragon fijn en voeg toe aan de ringetjes sjalot.
-                      9. Spoel de kropsla in koud water, zwier uit en meng onder de rest van de salade.
-                      10. Haal de frieten uit de diepvriezer en bak de frieten krokant in de hete friteuse.
-                      11. Serveer de steak tartaar met de kappertjes, de frieten en de salade.
-                    </p>
-                  </div>
-                </div>
-              </div>  */}
-
-
-
-
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,54 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import { allRecipeTypes } from "../lib/api"
 
-const Home = () => {
+const Home = ({ types }) => {
   return (
     <div className={styles.container}>
+      {console.log(types)}
       <div className={styles.wrapper_front_page} >
         <div className={styles.content_wrapper}>
-        <div className={styles.upper_part}>
-          <h1>Best recipes</h1>
-          <p>By Gilles Danneels</p>
+          <div className={styles.upper_part}>
+            <h1>Best recipes</h1>
+            <p>By Gilles Danneels</p>
+          </div>
+          <div className={styles.lower_part}>
+            <a href="#catergory"><button className={styles.read_more_btn}>Read more</button></a>
+          </div>
         </div>
-        <div className={styles.lower_part}>
-          <a href="#catergory"><button className={styles.read_more_btn}>Read more</button></a>
-        </div>
-       </div>
       </div>
 
       <div id="catergory" className={styles.category_page} $>
         <div className={styles.wrapper_category_page_content}>
-        <h1>Categories</h1>
-        <div className={styles.category_types}>
-          <Link href="/vegan" key="vegan">
-            <div className={styles.wrapper_category_card}>
-              <div  className={`${styles["category_card"]} ${styles["vegan_category_card"]}`}>
-                <h2>Vegan recipes</h2>
-              </div>
-            </div>
-          </Link>
-          <Link href="/veggie" key="veggie">
-            <div className={styles.wrapper_category_card}>
-              <div  className={`${styles["category_card"]} ${styles["veggie_categroy_card"]}`}>
-                <h2>Veggie recipes</h2>
-              </div>
-            </div>
-          </Link>
-          <Link href="/meat" key="meat">
-            <div className={styles.wrapper_category_card}>
-              <div  className={`${styles["category_card"]} ${styles["meat_categroy_card"]}`}>
-              <h2>Meat recipes</h2>
-              </div>
-            </div>
-          </Link>
+          <h1>Categories</h1>
+          <div className={styles.category_types}>
+            {
+              types.map(type => (
+                <Link href={`/${type.typeName}`} key={`${type.typeName}`}>
+                  <div className={styles.wrapper_category_card}>
+                    {console.log(type.typeName)}
+                    <div className={`${styles["category_card"]} ${styles[`${type.typeName}_category_card`]}`}>
+                      <h2>{type.typeName} recipes</h2>
+                    </div>
+                  </div>
+                </Link>
+              ))
+            }
+          </div>
         </div>
-        </div>
-        
+
       </div>
     </div>
-    
-    
-    
+
+
+
   )
+}
+
+export async function getStaticProps() {
+  const types = await allRecipeTypes()
+
+  return { props: { types } }
 }
 
 export default Home
